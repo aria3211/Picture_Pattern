40203d2d9434abef2d3dacf69c5c7ce3315d5423
aria <aryashahbazi@gmail.com>
2021-10-06 16:23:10 +0330

    main.cpp is update

--- app/main.cpp
+++ app/main.cpp
@@ -2,11 +2,37 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include "Foo.h"
+
+using namespace std;
+
+
+void PicturePattern(int x);
+
+
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
+  PicturePattern(7);
 
-  return 0;
+}
+
+
+void PicturePattern(int x)
+{
+  
+  for (int i = x; i >= 1; i--)
+  {
+    for (int j = 1; j <= x+5-i; j++) {
+      cout << " ";
+    }
+    for (int j = 1; j <= i; j++) {
+      cout <<j;
+    }
+    for (int j = i-1; j >= 1; j--) {
+      cout << j;
+    }
+
+
+    cout << endl;
+
+  }
 }
